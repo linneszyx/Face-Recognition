33b317bdb15e2d9a3c18d5136f2c4d9e18e40a58
linneszyx <itssuryanshcooldude346@gmail.com>
2023-02-19 20:16:04 +0530

    getting sq around face

--- src/Components/FaceRecognise/FaceRecognise.js
+++ src/Components/FaceRecognise/FaceRecognise.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./FaceRecognise.css";
-const FaceRecognise = ({ imageUrl,box }) => {
+const FaceRecognise = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img id='inputimage' 
-        src={imageUrl} 
-        alt="" 
-        width='500px' 
-        height='auto' 
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt=""
+          width="500px"
+          height="auto"
         />
-        <div className="bounding-box">
-
-        </div>
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
